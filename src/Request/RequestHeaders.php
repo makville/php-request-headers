01785d1607f4120e36386bc4d75e2b5ed5492200
Ayo Makanjuola <ayomakanjuola@hotmail.com>
2016-05-07 13:46:30 +0100

    This is the working version for unit testing

--- src/Request/RequestHeaders.php
+++ src/Request/RequestHeaders.php
@@ -1,22 +1,27 @@
 <?php
+
 class RequestHeaders {
 
-	public function __construct () {
-	
-	}
-	
-	public function getRequestHeader ($key, $part = null ) {
-		$header;
-		$requestHeaders = getallheaders();
-        $headers = $requestHeaders[$key];
-        $parts = explode(',', $headers);
-        foreach ($parts as $part ) {
-            if (strpos($part, $part . ' ') !== false ) {
-                $header = trim($part);
-                break;
+    public static function getRequestHeader($key, $prefix = null) {
+        $requestHeaders = getallheaders();
+        if (!is_null($key)) {
+            $headers = $requestHeaders[$key];
+            $parts = explode(',', $headers);
+            if (is_array($parts)) {
+                foreach ($parts as $part) {
+                    if (strpos($part, $prefix . ' ') !== false) {
+                        $header = trim($part);
+                        break;
+                    }
+                }
             }
         }
         return $header;
-	}
-}
+    }
 
+    public static function getHeaderValue($header) {
+        $parts = explode(' ', $header);
+        return array_pop($parts);
+    }
+
+}
