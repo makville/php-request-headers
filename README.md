ea712a7b88b9dcd430057a704ac57f2620ad6251
Ayo Makanjuola <ayomakanjuola@hotmail.com>
2016-06-26 10:29:38 +0100

    Namespaced

---
+++ README.md
@@ -0,0 +1,1 @@
+We will read me
--- src/Request/RequestHeaders.php
+++ src/Request/RequestHeaders.php
@@ -1,11 +1,13 @@
 <?php
+namespace Makville\RequestHeaders\Request;
 
 class RequestHeaders {
 
     public static function getRequestHeader($key, $prefix = null) {
-        $requestHeaders = getallheaders();
+        $requestHeaders = self::getAllHeaders();
+        $header = null;
         if (!is_null($key)) {
-            $headers = $requestHeaders[$key];
+            $headers = (isset($requestHeaders[$key])) ? $requestHeaders[$key] : null;
             $parts = explode(',', $headers);
             if (is_array($parts)) {
                 foreach ($parts as $part) {
@@ -24,4 +26,16 @@ class RequestHeaders {
         return array_pop($parts);
     }
 
+    public static function getAllHeaders() {
+        $headers = '';
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        if (function_exists('apache_request_headers')) {
+            return array_merge($headers, apache_request_headers());
+        }
+        return $headers;
+    }
 }
